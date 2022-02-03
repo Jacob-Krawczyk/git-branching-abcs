538b01036efca7a2754c0512106e340e9621a7bd
Jacob Krawczyk <krawczj@email.sc.edu>
2022-02-03 13:57:46 -0500

    Added Hello

---
+++ Branching.java
@@ -0,0 +1,6 @@
+public class Branching {
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+}git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
